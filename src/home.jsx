acd9516e95835057455f85ec6fd80847f401d563
Guaaan <72053761+Guaaan@users.noreply.github.com>
2020-12-17 20:25:23 -0300

    first commit

---
+++ src/home.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Alert from './alert';
+
+export const Home = (props) => {
+    let names = ["Maria", "Ana", "Pedro", "Miguel", "Moises", "Juan", "Daniel"];
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <Alert text="Este es mi Alert" show={true} names={names} />
+                </div>
+            </div>
+            {
+                props.show ?
+                    (
+                        <div className="row">
+                            <div className="col-md-12">
+                                <h1>Hola desde React</h1>
+                            </div>
+                        </div>
+                    ) : (
+                        <div className="row">
+                            <div className="col-md-12">
+                                <h1>Mostrando Contenido Oculto</h1>
+                            </div>
+                        </div>
+                    )
+            }
+        </div>
+    )
+}
+
+export default Home
--- src/index.js
+++ src/index.js
@@ -15,3 +15,31 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
+
+
+function SimpleCounter(props){
+    return (<div className="bigCounter">
+        <div className="calendar"><i className="far fa-clock"></i></div>
+        <div className="five">{props.digitFive}</div>
+        <div className="four">{props.digitFour}</div>
+        <div className="three">{props.digitThree}</div>
+        <div className="two">{props.digitTwo}</div>
+        <div className="one">{props.digitOne}</div>
+    </div>);
+}
+
+let counter = 0;
+setInterval(function(){
+    const four = math.floor(counter = 1000)
+    const three = math.floor(counter = 100)
+    const two = math.floor(counter = 10)
+    const one = math.floor(counter = 1)
+    console.log(one, two, three, four)
+
+})
+
+ReactDOM.render(<SimpleCounter show={true} />, document.querySelector("#root"));
+<SimpleCounter />
